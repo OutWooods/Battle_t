7b20665d956a73a5820c6cfe21b3f28fc1b01b53
Tagrand <thomas.a.grand@gmail.com>
2018-01-18 12:09:01 +0000

    refactor switch_turns feature test

--- spec/features/switch-turns_spec.rb
+++ spec/features/switch-turns_spec.rb
@@ -1,13 +1,16 @@
 feature 'switch turns' do
+
+   background do
+     sign_in_and_play
+     click_button("Switch turns")
+   end
+
   scenario 'change attacker on switch turns' do
-    sign_in_and_play
-    click_button("Switch turns")
     expect(page).to have_content("Attacker is now Chris")
+    expect(page).to_not have_content("Attacker is now Jennifer")
   end
 
-  scenario '' do
-    sign_in_and_play
-    click_button("Switch turns")
+  scenario 'defender should have changed' do
     click_button("Attack!")
     expect(page).to have_content("Jennifer's HP is now 90.")
   end
