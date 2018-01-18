1263e069d6d942792c69f15c8137d7a0bbe03df8
Tagrand <thomas.a.grand@gmail.com>
2018-01-18 11:58:03 +0000

    add switch players option

--- lib/game.rb
+++ lib/game.rb
@@ -2,14 +2,26 @@ require_relative "player"
 
 class Game
 
-  attr_reader :player_1, :player_2
+  attr_reader :players
 
   def initialize(player_1, player_2)
-    @player_1 = player_1
-    @player_2 = player_2
+    @players = [player_1, player_2]
+  end
+
+  def attacker
+    @players.first
+  end
+
+  def defender
+    @players.last
   end
 
   def hit(opponent)
     opponent.receive_damage
   end
+
+  def switch
+    @players.reverse!
+  end
+
 end
