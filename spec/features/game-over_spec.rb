f99c053f255d56800414c3d314620d6459ea8b5c
Tagrand <thomas.a.grand@gmail.com>
2018-01-18 13:10:45 +0000

    test players game doesn't end before a player dies

--- spec/features/game-over_spec.rb
+++ spec/features/game-over_spec.rb
@@ -8,11 +8,10 @@ feature 'displays hitpoints' do
     expect(page).to have_content("Game over")
   end
 
-  # add test to make sure health doesn't go into negatives
-  # scenario 'when one player dies, game says its over' do
-  #   sign_in_and_play
-  #   10.times{click_button("Attack!")
-  #   click_button("Next move...!") }
-  #   expect(page).to have_content("Game over")
-  # end
+  scenario 'when player is close to death, the game is not over' do
+    sign_in_and_play
+    9.times{click_button("Attack!")
+    click_button("Next move...!") }
+    expect(page).to_not have_content("game over")
+  end
 end
